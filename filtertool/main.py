633f7ac3e56fd20020cbf99004e68ca2034a92fb
otiai10 <otiai10@gmail.com>
2017-04-18 13:12:06 +0900

    Enalbe to control filter parameters by CLI args

--- filtertool/main.py
+++ filtertool/main.py
@@ -1,13 +1,13 @@
 import csv
-import sys
+import sys, getopt
 from filter import Filter
 
-def scan(f, fltr):
+def scan(f, fltr, verbose=False, trial=False):
 	pool = []
 	for i, line in enumerate(f.readlines()):
-		if i != 0 and i % 500 == 0: sys.stderr.write('.')
-		if i % 100000 == 0: sys.stderr.write("{0}\n".format(i))
-		# if i != 0 and i % 100000 == 0: break
+		if verbose and i != 0 and i % 500 == 0: sys.stderr.write('.')
+		if verbose and i != 0 and i % 100000 == 0: sys.stderr.write("{0}\n".format(i))
+		if trial   and i != 0 and i % 100000 == 0: break
 		try:
 			for v in fltr.match(line): pool += [v.compose()]
 		except Exception as err:
@@ -15,17 +15,35 @@ def scan(f, fltr):
 			raise err
 	return pool
 
-def write_result(result):
-	with open("result.csv", "w") as f:
+def write_result(output_file, result):
+	with open(output_file, "w") as f:
 		csv.writer(f, delimiter="\t").writerows(result)
 		
 
-def filtertool_main():
-	fltr = Filter()
-	with open("test.pileup", "r") as f:
-		result = scan(f, fltr)
-		sys.stderr.write("FOUND: {0}\n".format(len(result)))
-		write_result(result)
+def filtertool_main(argv):
+	opts, args = getopt.getopt(argv, 'i:o:d:c:f:vt', [
+		"input=", "output=", "depth=", "count=", "freq=", "verbose", "trial"
+	])	
+	input_file = ''
+	output_file = 'result.vcf'
+	depth = 10
+	count = 3
+	freq = 0.2
+	varbose = False
+	trial = False
+	for opt, arg in opts:
+		if opt in ("-v", "--verbose"): verbose = True
+		elif opt in ("-t", "--trial"): trial = True
+		elif opt in ("-i", "--input"): input_file = arg
+		elif opt in ("-o", "--output"): output_file = arg
+		elif opt in ("-d", "--depth"): depth = int(arg)
+		elif opt in ("-c", "--count"): count = int(arg)
+		elif opt in ("-f", "--freq"): freq = float(arg)
+	fltr = Filter(depth, count, freq)
+	with open(input_file, "r") as f:
+	 	result = scan(f, fltr, verbose, trial)
+	 	sys.stderr.write("FOUND: {0}\n".format(len(result)))
+	 	write_result(output_file, result)
 
 if __name__ == "__main__":
-	filtertool_main()
+	filtertool_main(sys.argv[1:])
