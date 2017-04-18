240fd45bf176e75fb8f3e6773391cee277b3be99
Hiromu OCHIAI <otiai10@gmail.com>
2017-04-18 15:16:50 +0900

    Scan packages under project

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
 	name='filtertool',
 	version='0.0.1',
+    packages=find_packages(),
 	entry_points={
 		'console_scripts': 'filtertool = filtertool.main:filtertool_main'
 	},
