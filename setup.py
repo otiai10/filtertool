dbb0b60f8849c61d735d6812b05a128028ed50c7
Hiromu OCHIAI <otiai10@gmail.com>
2017-05-15 14:11:09 +0900

    v0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='filtertool',
-    version='0.0.1',
+    version='0.0.2',
     packages=find_packages(),
     entry_points={
         'console_scripts': 'filtertool = filtertool.main:filtertool_main'
