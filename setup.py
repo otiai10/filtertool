3deef8cfb4ca17ed5bb8080d4c8ff995463f61fb
Hiromu OCHIAI <otiai10@gmail.com>
2017-08-07 15:20:22 +0900

    Bump up version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='filtertool',
-    version='0.0.3',
+    version='0.0.4',
     packages=find_packages(),
     entry_points={
         'console_scripts': 'filtertool = filtertool.main:filtertool_main'
